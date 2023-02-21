ab3d221d80c607be8457980eff9c170af9eb4c0e
Theo <32236823+piggeywig2000@users.noreply.github.com>
2023-02-21 07:19:06 +0000

    Moved frontend into www and added backend

---
+++ forsen_minecraft_time.py
@@ -0,0 +1,59 @@
+import datetime as dt
+from flask import Flask, request, g
+from flask_cors import CORS
+import json
+import mysql.connector
+
+sql_get_latest_time = "SELECT id_date, game_time, real_time FROM times ORDER BY id_date DESC LIMIT 1"
+sql_get_time_history = "SELECT id_date, game_time, real_time FROM times WHERE id_date >= %s AND id_date < %s ORDER BY id_date DESC"
+
+app = Flask("forsen_minecraft_time")
+CORS(app)
+
+with open("secrets.json") as secret_json_file:
+    r_db_pw = json.load(secret_json_file)["database_r_pw"]
+
+def get_db():
+    if "db" not in g:
+        g.db = mysql.connector.connect(user='forsen_minecraft_r', password=r_db_pw, host='127.0.0.1', database='forsen_minecraft')
+    return g.db
+
+def close_db(e=None):
+    db = g.pop("db", None)
+    if db is not None:
+        db.close()
+
+app.teardown_appcontext(close_db)
+
+@app.route("/time/latest", methods=["GET"])
+def get_latest_time():
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute(sql_get_latest_time)
+    (id_date, game_time, real_time) = cursor.fetchone()
+    cursor.close()
+    db.commit()
+    return { "date": id_date.isoformat(), "igt": game_time.total_seconds(), "rta": real_time.total_seconds() }
+
+@app.route("/time/history", methods=["GET"])
+def get_time_history():
+    if "from" not in request.args or "to" not in request.args:
+        return "from or to missing", 400
+    try:
+        from_date = dt.datetime.fromisoformat(request.args.get("from"))
+        to_date = dt.datetime.fromisoformat(request.args.get("to"))
+    except ValueError:
+        return "from or to is not a date", 400
+    if to_date < from_date:
+        return "to date comes before from date", 400
+    if to_date - from_date > dt.timedelta(hours=1):
+        return "timespan between from and to is too large", 400
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute(sql_get_time_history, (from_date, to_date))
+    return_list = []
+    for (id_date, game_time, real_time) in cursor:
+        return_list.append({ "date": id_date.isoformat(), "igt": game_time.total_seconds(), "rta": real_time.total_seconds() })
+    cursor.close()
+    db.commit()
+    return return_list
---
+++ forsen_minecraft_time_scraper.py
@@ -0,0 +1,232 @@
+import datetime as dt
+import json
+from time import sleep
+#import threading
+
+import imageio.v3 as iio
+import mysql.connector
+import numpy as np
+#from PIL import Image
+import requests
+import streamlink
+from streamlink.plugins.twitch import TwitchM3U8Parser, TwitchM3U8
+from streamlink.stream.hls_playlist import load as load_hls_playlist
+
+RELOAD_TIME = 4
+DISTANCE_CUTOFF = 150
+
+digit_pixelmap = np.array([
+    [['t', 'b', 'b', 'b', 'b', 'b', 't'],
+     ['b', 'b', 'c', 'c', 'c', 'b', 'b'],
+     ['b', 'c', 'b', 'b', 'b', 'c', 'b'],
+     ['b', 'c', 'b', 'b', 'c', 'c', 'b'],
+     ['b', 'c', 'b', 'c', 'b', 'c', 'b'],
+     ['b', 'c', 'c', 'b', 'b', 'c', 'b'],
+     ['b', 'c', 'b', 'b', 'b', 'c', 'b'],
+     ['b', 'b', 'c', 'c', 'c', 'b', 'b'],
+     ['t', 'b', 'b', 'b', 'b', 'b', 't'],],
+
+    [['t', 't', 'b', 'b', 'b', 't', 't'],
+     ['t', 'b', 'b', 'c', 'b', 't', 't'],
+     ['t', 'b', 'c', 'c', 'b', 't', 't'],
+     ['t', 'b', 'b', 'c', 'b', 't', 't'],
+     ['t', 't', 'b', 'c', 'b', 't', 't'],
+     ['t', 't', 'b', 'c', 'b', 't', 't'],
+     ['b', 'b', 'b', 'c', 'b', 'b', 'b'],
+     ['b', 'c', 'c', 'c', 'c', 'c', 'b'],
+     ['b', 'b', 'b', 'b', 'b', 'b', 'b'],],
+
+    [['t', 'b', 'b', 'b', 'b', 'b', 't'],
+     ['b', 'b', 'c', 'c', 'c', 'b', 'b'],
+     ['b', 'c', 'b', 'b', 'b', 'c', 'b'],
+     ['b', 'b', 'b', 'b', 'b', 'c', 'b'],
+     ['t', 'b', 'b', 'c', 'c', 'b', 'b'],
+     ['b', 'b', 'c', 'b', 'b', 'b', 'b'],
+     ['b', 'c', 'b', 'b', 'b', 'c', 'b'],
+     ['b', 'c', 'c', 'c', 'c', 'c', 'b'],
+     ['b', 'b', 'b', 'b', 'b', 'b', 'b'],],
+
+    [['t', 'b', 'b', 'b', 'b', 'b', 't'],
+     ['b', 'b', 'c', 'c', 'c', 'b', 'b'],
+     ['b', 'c', 'b', 'b', 'b', 'c', 'b'],
+     ['b', 'b', 'b', 'b', 'b', 'c', 'b'],
+     ['t', 't', 'b', 'c', 'c', 'b', 'b'],
+     ['b', 'b', 'b', 'b', 'b', 'c', 'b'],
+     ['b', 'c', 'b', 'b', 'b', 'c', 'b'],
+     ['b', 'b', 'c', 'c', 'c', 'b', 'b'],
+     ['t', 'b', 'b', 'b', 'b', 'b', 't'],],
+
+    [['t', 't', 't', 'b', 'b', 'b', 'b'],
+     ['t', 't', 'b', 'b', 'c', 'c', 'b'],
+     ['t', 'b', 'b', 'c', 'b', 'c', 'b'],
+     ['b', 'b', 'c', 'b', 'b', 'c', 'b'],
+     ['b', 'c', 'b', 'b', 'b', 'c', 'b'],
+     ['b', 'c', 'c', 'c', 'c', 'c', 'b'],
+     ['b', 'b', 'b', 'b', 'b', 'c', 'b'],
+     ['t', 't', 't', 't', 'b', 'c', 'b'],
+     ['t', 't', 't', 't', 'b', 'b', 'b'],],
+
+    [['b', 'b', 'b', 'b', 'b', 'b', 'b'],
+     ['b', 'c', 'c', 'c', 'c', 'c', 'b'],
+     ['b', 'c', 'b', 'b', 'b', 'b', 'b'],
+     ['b', 'c', 'c', 'c', 'c', 'b', 'b'],
+     ['b', 'b', 'b', 'b', 'b', 'c', 'b'],
+     ['b', 'b', 'b', 't', 'b', 'c', 'b'],
+     ['b', 'c', 'b', 'b', 'b', 'c', 'b'],
+     ['b', 'b', 'c', 'c', 'c', 'b', 'b'],
+     ['t', 'b', 'b', 'b', 'b', 'b', 't'],],
+
+    [['t', 't', 'b', 'b', 'b', 'b', 't'],
+     ['t', 'b', 'b', 'c', 'c', 'b', 't'],
+     ['b', 'b', 'c', 'b', 'b', 'b', 't'],
+     ['b', 'c', 'b', 'b', 'b', 'b', 't'],
+     ['b', 'c', 'c', 'c', 'c', 'b', 'b'],
+     ['b', 'c', 'b', 'b', 'b', 'c', 'b'],
+     ['b', 'c', 'b', 'b', 'b', 'c', 'b'],
+     ['b', 'b', 'c', 'c', 'c', 'b', 'b'],
+     ['t', 'b', 'b', 'b', 'b', 'b', 't'],],
+
+    [['b', 'b', 'b', 'b', 'b', 'b', 'b'],
+     ['b', 'c', 'c', 'c', 'c', 'c', 'b'],
+     ['b', 'c', 'b', 'b', 'b', 'c', 'b'],
+     ['b', 'b', 'b', 'b', 'b', 'c', 'b'],
+     ['t', 't', 'b', 'b', 'c', 'b', 'b'],
+     ['t', 't', 'b', 'c', 'b', 'b', 't'],
+     ['t', 't', 'b', 'c', 'b', 't', 't'],
+     ['t', 't', 'b', 'c', 'b', 't', 't'],
+     ['t', 't', 'b', 'b', 'b', 't', 't'],],
+
+    [['t', 'b', 'b', 'b', 'b', 'b', 't'],
+     ['b', 'b', 'c', 'c', 'c', 'b', 'b'],
+     ['b', 'c', 'b', 'b', 'b', 'c', 'b'],
+     ['b', 'c', 'b', 'b', 'b', 'c', 'b'],
+     ['b', 'b', 'c', 'c', 'c', 'b', 'b'],
+     ['b', 'c', 'b', 'b', 'b', 'c', 'b'],
+     ['b', 'c', 'b', 'b', 'b', 'c', 'b'],
+     ['b', 'b', 'c', 'c', 'c', 'b', 'b'],
+     ['t', 'b', 'b', 'b', 'b', 'b', 't'],],
+
+    [['t', 'b', 'b', 'b', 'b', 'b', 't'],
+     ['b', 'b', 'c', 'c', 'c', 'b', 'b'],
+     ['b', 'c', 'b', 'b', 'b', 'c', 'b'],
+     ['b', 'c', 'b', 'b', 'b', 'c', 'b'],
+     ['b', 'b', 'c', 'c', 'c', 'c', 'b'],
+     ['t', 'b', 'b', 'b', 'b', 'c', 'b'],
+     ['t', 'b', 'b', 'b', 'c', 'b', 'b'],
+     ['t', 'b', 'c', 'c', 'b', 'b', 't'],
+     ['t', 'b', 'b', 'b', 'b', 't', 't'],],
+])
+digit_pixelmap = np.repeat(np.repeat(digit_pixelmap, 3, axis=1), 3, axis=2)
+
+sql_add_time = "INSERT INTO times (id_date, game_time, real_time) VALUES (%s, %s, %s)"
+
+with open("secrets.json") as secret_json_file:
+    rw_db_pw = json.load(secret_json_file)["database_rw_pw"]
+
+def compare_image_to_pixelmap(image, pixelmap, colour):
+    count = np.count_nonzero(pixelmap)
+    if count == 0:
+        return 0
+    diff = image - colour
+    dist = np.linalg.norm(diff, axis=2) * pixelmap
+    total = np.sum(dist / count)
+    return total
+
+def get_time_from_frame(frame, colour, debug=False):
+    images = [frame[:,0:21,:], frame[:,18:39,:], frame[:,42:63,:], frame[:,60:81,:], frame[:,84:105,:], frame[:,102:123,:], frame[:,120:141,:]]
+    image_place_value = [600000, 60000, 10000, 1000, 100, 10, 1]
+
+    milliseconds = 0
+    for idx_image, image in enumerate(images):
+        #Find most likely digit. If it's a close enough match, continue. If not, return None
+        best_num = 0
+        best_distance = float("inf")
+        best_colour_distance = 0
+        best_black_distance = 0
+        for idx_pm, pm in enumerate(digit_pixelmap):
+            colour_distance = compare_image_to_pixelmap(image, pm == 'c', colour)
+            black_distance = compare_image_to_pixelmap(image, pm == 'b', (0,0,0))
+            total_distance = colour_distance + black_distance
+            if total_distance < best_distance:
+                best_distance = total_distance
+                best_colour_distance = colour_distance
+                best_black_distance = black_distance
+                best_num = idx_pm
+
+        if best_distance > DISTANCE_CUTOFF:
+            return None
+        milliseconds += best_num * image_place_value[idx_image]
+    
+    if (debug):
+        print(str((str(dt.timedelta(milliseconds=milliseconds)), best_distance, best_colour_distance, best_black_distance)))
+
+    return dt.timedelta(milliseconds=milliseconds)
+
+def main_loop():
+    session = streamlink.Streamlink()
+    session.set_plugin_option("twitch", "low-latency", True)
+    session.set_plugin_option("twitch", "disable-ads", True)
+    streams = session.streams("https://www.twitch.tv/forsen")
+    if "1080p60" not in streams:
+        raise Exception("forsen not live")
+    stream = streams["1080p60"]
+    
+    start_time = dt.datetime.utcnow()
+    iterations = 0
+    while True:
+        target_time = start_time + dt.timedelta(seconds=RELOAD_TIME*iterations)
+        sleep_time = target_time - dt.datetime.utcnow()
+        if sleep_time <= dt.timedelta():
+            print("Reload overdue, not sleeping...")
+            start_time = dt.datetime.utcnow()
+            iterations = 0
+        else:
+            print(f"Sleeping for {sleep_time.total_seconds()}")
+            sleep(sleep_time.total_seconds())
+        iterations += 1
+
+        m3u8_response = requests.get(stream.url).text
+        m3u8_playlist = load_hls_playlist(m3u8_response, parser=TwitchM3U8Parser, m3u8=TwitchM3U8)
+        latest_segment = m3u8_playlist.segments[-1]
+        if latest_segment.ad:
+            print("Has ad, skipping...")
+            continue
+        segment_data = requests.get(latest_segment.uri).content
+        # meta = iio.immeta(segment_data, index=None, extension=".ts")
+        # last_frame_idx = round(meta["duration"] * meta["fps"]) - 1
+        last_frame = iio.imread(segment_data, index=0, extension=".ts")
+
+        #Image.fromarray(last_frame[81:108,1749:1890,:]).save(f"test-{latest_segment.date.isoformat().replace(':', '-')}.bmp")
+
+        game_time = get_time_from_frame(last_frame[81:108,1749:1890,:], (255, 255, 85), True)
+        real_time = get_time_from_frame(last_frame[33:60,1749:1890,:], (85, 255, 255))
+        if game_time == None or real_time == None:
+            continue
+        #print(latest_segment.date)
+        
+        cnx_rw = mysql.connector.connect(user='forsen_minecraft_rw', password=rw_db_pw, host='127.0.0.1', database='forsen_minecraft')
+        try:
+            cursor = cnx_rw.cursor()
+            cursor.execute(sql_add_time, (latest_segment.date, game_time, real_time))
+            cursor.close()
+            cnx_rw.commit()
+        finally:
+            cnx_rw.close()
+
+def thread_entry_point():
+    while True:
+        try:
+            main_loop()
+        except Exception as e:
+            if str(e) != "forsen not live":
+                print("Error in thread: " + str(e))
+        sleep(10)
+
+# x = threading.Thread(target=thread_entry_point)
+# x.start()
+thread_entry_point()
+
+# test_i = Image.open("wrong.bmp")
+# test_a = np.array(test_i)
+# game_time = get_time_from_frame(test_a, (255, 255, 85))
+# print(game_time)
